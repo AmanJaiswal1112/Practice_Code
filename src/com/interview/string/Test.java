597b1377f4bd510697600a0d59f04cceb075516c
amanjaiswal1112 <aman.jaiswal.1112@gmail.com>
2024-04-06 14:36:26 +0530

    Added String prog

--- src/com/interview/string/Test.java
+++ src/com/interview/string/Test.java
@@ -1,36 +1,105 @@
-package com.interview.string;
-
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+class Account {
+
+    private String accountId;
+    private String accountHolderName;
+    private double balance;
+    private List<String> transactionHistory;
+
+    public Account(String accountId, String accountHolderName) {
+        this.accountId = accountId;
+        this.accountHolderName = accountHolderName;
+        this.balance = 0;
+        this.transactionHistory = new ArrayList<>();
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+
+    public void setAccountHolderName(String accountHolderName) {
+        this.accountHolderName = accountHolderName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public List<String> getTransactionHistory() {
+        return transactionHistory;
+    }
+
+    public void setTransactionHistory(List<String> transactionHistory) {
+        this.transactionHistory = transactionHistory;
+    }
+}
+
+
+class OnlineBankingSystem{
+    private Map<String, Account> accounts;
+
+    public OnlineBankingSystem(Map<String, Account> accounts) {
+        this.accounts = accounts;
+    }
+
+    public void createAccount(String accountId, String accountHolderName){
+        if(!accounts.containsKey(accountId)){
+            Account account = new Account(accountId, accountHolderName);
+            accounts.put(accountId, account);
+            System.out.println("Account created successfully");
+        }else{
+            System.out.println("Account id already exist");
+        }
+    }
+
+
+    public void deposite(String accountID, double amount){
+        Account account = accounts.get(accountID);
+        if (account != null) {
+            account.deposite(amount);
+            System.out.println("DEposite completed");
+        }else
+        {
+            System.out.println("Account not found");
+        }
+    }
+
+    public void deposite(String accountID, double amount){
+        Account account = accounts.get(accountID);
+        if (account != null) {
+            account.withdraw(amount);
+            System.out.println("withdraw completed");
+        }else
+        {
+            System.out.println("Account not found");
+        }
+    }
+
+    public void transfer(String senderId , String receipentId , double amount){
+
+    }
+
+
+}
+
 
 public class Test {
     public static void main(String[] args) {
-        Integer[] arr = {14,16,18,23,45,98};
-        int element =23;
-        
-        Integer index = findIndex(arr, element);
-        System.out.println(index);
-    }
-
-    private static Integer findIndex(Integer[] arr, int element) {
-
-        int left = 0, right = arr.length;
-
-        while (left <= right) {
-            int mid = (left + right) / 2;
-
-
-            if (arr[mid] == element) {
-                return mid;
-            }
-
-            if (arr[mid] < element) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-
-        }
-        return -1;
+        System.out.println("Herllo");
     }
 }
