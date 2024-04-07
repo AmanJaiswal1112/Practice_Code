fab2e79d02bea5def2e71951187be5c6f82d227a
AmanJaiswal1112 <Ecsbulab@123>
2024-04-07 19:25:34 +0530

    Added few code

--- src/com/interview/string/pppp.java
+++ src/com/interview/string/pppp.java
@@ -1,7 +1,27 @@
 package com.interview.string;
 
+ class outer
+{
+    public static int temp = 1;
+    private static int temp2 = 1;
+    public  int temp3 = 1;private int temp4 = 1;
+
+    public static class  inner{
+
+        private int sum(){
+            return (temp+temp2);
+        }
+
+    }
+
+
+}
+
+
 public class pppp {
     public static void main(String[] args) {
-        System.out.println("hello");
+        int p = 5;
+        System.out.println("h");
+
     }
 }
